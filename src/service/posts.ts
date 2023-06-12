d5ac56903b34aa9fc47b26667b5f341f82c4d3d7
ryanjhlee87 <108978592+ryanjhlee87@users.noreply.github.com>
2023-06-12 14:45:34 +0900

    Add prev/next buttons

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,11 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
@@ -35,12 +39,15 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-  const metadata = await getAllPosts().then(posts =>
-    posts.find(post => post.path === fileName)
-  );
-  if (!metadata) throw new Error(`${fileName} data does not exist!`);
+  const posts = await getAllPosts();
+  const post = posts.find(post => post.path === fileName);
 
+  if (!post) throw new Error(`${fileName} data does not exist!`);
+
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length ? posts[index + 1] : null;
   const content = await readFile(filePath, 'utf-8');
 
-  return { ...metadata, content };
+  return { ...post, content, next, prev };
 }
